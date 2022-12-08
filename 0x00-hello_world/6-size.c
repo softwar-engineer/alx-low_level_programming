c0626fc2342be0441a88563983001ebe42ba2436
Nebiyou Yemam <nebiyou.yemam@yahoo.com>
2022-12-08 03:33:43 -0800

    tasl 6 sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: terminates after code completion
+ *
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of an int: %d byte(s)\n", sizeof(long int));
+	printf("Size of an int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of an int: %d byte(s)\n", sizeof(float));
+	return (0);
+}
