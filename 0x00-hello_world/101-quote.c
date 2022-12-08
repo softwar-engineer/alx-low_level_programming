32ab1abf29aebd2151ed74706c22420e626563af
Nebiyou Yemam <nebiyou.yemam@yahoo.com>
2022-12-08 04:04:28 -0800

    task 8 UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 when code terminates
+ *
+ */
+
+int main(void)
+{
+	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
