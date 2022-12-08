5e628ff09c59299d5089bc8f5a8b4ffac67837ea
Nebiyou Yemam <nebiyou.yemam@yahoo.com>
2022-12-08 02:46:33 -0800

    task 5 Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * mian - entry point
+ *
+ * Return: ends after code completion
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
