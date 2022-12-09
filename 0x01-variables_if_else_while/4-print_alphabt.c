702651ab9793185b57536c871ea7c5463a8e1120
Nebiyou Yemam <nebiyou.yemam@yahoo.com>
2022-12-08 23:48:50 -0800

    task 4 prints incomplete alphabet without e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 if completed successfully
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i == 101 || i == 113)
+			continue;
+		else
+		{
+			putchar(i);
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
