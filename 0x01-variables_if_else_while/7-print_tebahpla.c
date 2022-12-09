c37da9fba6bdd260f7fab6fab0b89e2c90a4ce8b
Nebiyou Yemam <nebiyou.yemam@yahoo.com>
2022-12-09 00:07:24 -0800

    task 7 smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 if successfully completed
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
