01e1400030feab5c3003beef0d8d68b6637fb81e
Nebiyou Yemam <nebiyou.yemam@yahoo.com>
2022-12-09 03:32:47 -0800

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,12 +11,16 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 57; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
+		if (i == 57)
+		{
+			continue;
+		}
 		putchar(44);
 		putchar(32);
 	}
-	putchar(i);
+	putchar('\n');
 	return (0);
 }
