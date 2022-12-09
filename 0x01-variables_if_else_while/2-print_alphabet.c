8b7df76d64c321a781fc51eea3aa1ba9b5a53f9e
Nebiyou Yemam <nebiyou.yemam@yahoo.com>
2022-12-08 23:29:56 -0800

    task 2 print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Retrun: 0 if successfully completed
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
