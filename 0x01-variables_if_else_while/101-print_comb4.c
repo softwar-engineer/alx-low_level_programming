71fff37ea4edc7695d4df23123a073c4bb374631
Nebiyou Yemam <nebiyou.yemam@yahoo.com>
2022-12-09 03:58:28 -0800

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,11 +11,11 @@ int main(void)
 {
 	int first_digit, second_digit, third_digit;
 
-	for (first_digit = 0; first_digit < 8; first_digit++)
+	for (first_digit = 0; first_digit < 9; first_digit++)
 	{
-		for (second_digit = first_digit + 1; second_digit < 8; second_digit++)
+		for (second_digit = first_digit + 1; second_digit < 9; second_digit++)
 		{
-			for (third_digit = second_digit + 1; third_digit < 9; third_digit++)
+			for (third_digit = second_digit + 1; third_digit < 10; third_digit++)
 			{
 				putchar(first_digit + '0');
 				putchar(second_digit + '0');
