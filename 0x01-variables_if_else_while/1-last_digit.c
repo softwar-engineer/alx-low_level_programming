4ecbf9c10ec153a2b8ccada3d3993c5539230ee3
Nebiyou Yemam <nebiyou.yemam@yahoo.com>
2022-12-09 04:07:14 -0800

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,7 +28,7 @@ int main(void)
 
 	else
 	{
-		printf("Last digit of %d is %d and less than 6 and not 0", n, last_digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, last_digit);
 	}
 	putchar('\n');
 	return (0);
