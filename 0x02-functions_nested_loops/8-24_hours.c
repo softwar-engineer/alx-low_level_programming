b0dc3eb3cad0260c0b6cdce37db6f617c3ca52ad
Nebiyou Yemam <nebiyou.yemam@yahoo.com>
2022-12-13 04:20:55 -0800

    task 8 jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints minutes in 24 hours
+ *
+ * Return: 0 if successful
+ */
+
+void jack_bauer(void)
+{
+	int hr, min;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar(hr / 10 + '0');
+			_putchar(hr % 10 + '0');
+			_putchar(58);
+			_putchar(min / 10 + '0');
+			_putchar(min % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
