7eb7eb38703d8454d3d52de8932b0c1a5e075751
Nebiyou Yemam <nebiyou.yemam@yahoo.com>
2022-12-13 02:42:48 -0800

    task 7 prints last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,19 @@
  */
 
 int print_last_digit(int n)
-{	
-	_putchar((n % 10) + '0'); 
-	return (0);
+{
+	int last_digit;
+
+	if (n < 0)
+	{
+		last_digit = (-1 * (n % 10));
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
+	else
+	{
+		last_digit = n % 10;
+		_putchar(n + '0');
+		return (last_digit);
+	}
 }
