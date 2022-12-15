c2e45bd8569b2143ac39d21d1309a01b1c97c755
Nebiyou Yemam <nebiyou.yemam@yahoo.com>
2022-12-14 22:49:49 -0800

    task 0 testing main function

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 /* more headers goes there */
 /**
  * main - entry point
@@ -9,12 +8,10 @@
  *
  */
 /* betty style doc for function main goes there */
-int main(void)
+
+void positive_or_negative(int n)
 {
 	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
