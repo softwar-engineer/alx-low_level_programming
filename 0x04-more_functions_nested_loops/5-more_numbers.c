76f66382c6e0d9473655d9e7688c245fcbbaf687
Nebiyou Yemam <nebiyou.yemam@yahoo.com>
2022-12-15 03:16:08 -0800

    task 5 prints from 1 to 14 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 1 t0 14 10 times
+ *
+ * Return: 0 if completed successfully
+ */
+
+void more_numbers(void)
+{
+	int i, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (k = 1; k < 15; k++)
+		{
+			if (k < 10)
+			{
+				_putchar(k + '0');
+			}
+			else
+			{
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
