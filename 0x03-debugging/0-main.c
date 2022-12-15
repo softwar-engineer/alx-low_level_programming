de67c32b935742e9d930ce700c6d7f73f3e59b60
Nebiyou Yemam <nebiyou.yemam@yahoo.com>
2022-12-14 22:44:23 -0800

    task 0 testing main function

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,31 @@
-#include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
-
+ * main - entry point
+ *
+ * Return: 0 if completed successfully
+ *
+ */
+/* betty style doc for function main goes there */
 int main(void)
 {
-	int i, k;
+	int n;
 
-	i = 98;
-	positive_or_negative(i);
-	k = 0;
-	
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", 0);
+	}
 	return (0);
 }
