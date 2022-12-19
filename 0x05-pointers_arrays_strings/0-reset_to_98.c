3ce49904d931e4effb2014429d7001986e83d543
Nebiyou Yemam <nebiyou.yemam@yahoo.com>
2022-12-19 01:02:37 -0800

    task 0 98 battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes value to 98
+ * @n: arg from test function
+ * Return: 0 if successfully completed
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
