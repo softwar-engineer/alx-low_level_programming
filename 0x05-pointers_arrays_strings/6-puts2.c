bf015bf9a9ce1c2b6fd9eaedf5eafbab98bc188e
Nebiyou Yemam <nebiyou.yemam@yahoo.com>
2022-12-19 05:52:54 -0800

    task 6 puts every other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,14 +8,15 @@
 
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
+		{
 			_putchar(str[i]);
-		else
-			continue;
+		}
+		i++;
 	}
-	_putchar('\n');
+	_putchar('\n');	
 }
