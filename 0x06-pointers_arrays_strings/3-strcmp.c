27c58be77de5f95034b48c9baac602d33119703f
Nebiyou Yemam <nebiyou.yemam@yahoo.com>
2022-12-23 03:29:59 -0800

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,32 +1,21 @@
 #include "main.h"
+
 /**
- * _strcmp - compares strings
- * @s1: arg1 from test function
- * @s2: arg2 from test function
- * Return: 0 if completed successfully
+ * _strcmp - compare two strings.
+ *@s1: string 1.
+ *@s2: string 2.
+ *Return: int.
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int length1, length2;
+	int i = 0, cmp = 0;
 
-	for (length1 = 0; s1[length1] != '\0'; length1++)
+	while (s1[i] != '\0' && s2[i] != '\0' && cmp == 0)
 	{
+		cmp = s1[i] - s2[i];
+		i++;
 	}
-	for (length2 = 0; s2[length2] != '\0'; length2++)
-	{
-	}
-	if (length1 > length2)
-	{
-		return (1);
-	}
-	else if (length1 < length2)
-	{
-		return (-1);
-	}
-	else if (length1 == length2)
-	{
-		return (0);
-	}
-	return (0);
+
+	return (cmp);
 }
