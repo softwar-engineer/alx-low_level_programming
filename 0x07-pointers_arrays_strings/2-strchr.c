d687dbc65578edbbc5ba09bd71071c559df19b67
Nebiyou Yemam <nebiyou.yemam@yahoo.com>
2023-01-02 02:19:01 -0800

    task 2 strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - return the first occurrence of a character
+ * @s: reference string to be checked in
+ * @c: character to be checked
+ * Return: returns the address of the first occurrence of the char
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+		else if (s[i] != c && s[i] == '\0')
+		{
+			return (NULL);
+		}
+	}
+
+	return (0);
+}
