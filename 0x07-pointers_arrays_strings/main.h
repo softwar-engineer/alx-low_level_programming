beb3154198541716f088ace834dedac3cdcb362c
Nebiyou Yemam <nebiyou.yemam@yahoo.com>
2023-01-02 22:42:20 -0800

    edited main.h file

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+void set_string(char **s, char *to);
 #endif /*MAIN_H*/
